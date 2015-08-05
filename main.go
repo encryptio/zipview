3468fe38edba9a1562ce58063bea3b2c0713c639
Chris Kastorff <encryptio@gmail.com>
2015-08-05 13:29:53 -0700

    lots of work

--- main.go
+++ main.go
@@ -1,61 +1,31 @@
 package main
 
 import (
-	"archive/zip"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"sort"
+	"strings"
 
-	"honnef.co/go/js/dom"
+	"github.com/gopherjs/gopherjs/js"
 )
 
-func addFile(f *zip.File) {
-	rc, err := f.Open()
-	if err != nil {
-		log.Printf("Couldn't open file in zip: %v", err)
-		return
-	}
-	defer rc.Close()
+func urlChannel() <-chan string {
+	ch := make(chan string, 1)
 
-	data, err := ioutil.ReadAll(rc)
-	if err != nil {
-		log.Printf("Couldn't read file in zip: %v", err)
-		return
+	handleHashChange := func() {
+		hash := js.Global.Get("location").Get("hash").String()
+		hash = strings.TrimPrefix(hash, "#")
+		ch <- hash
 	}
+	handleHashChange()
 
-	dataURL := toDataURL(http.DetectContentType(data), data)
-	doc := dom.GetWindow().Document()
-	img := doc.CreateElement("img")
-	img.SetAttribute("src", dataURL)
-	body := doc.QuerySelector("body")
-	body.AppendChild(img)
+	js.Global.Call("addEventListener", "hashchange", handleHashChange, false)
+
+	return ch
 }
 
 func main() {
-	url := "ookami.zip"
+	urls := urlChannel()
 
-	r, err := OpenHTTPReader(url)
-	if err != nil {
-		log.Printf("Couldn't open HTTP reader: %v", err)
+	viewer := NewViewer()
+	for url := range urls {
+		viewer.SetURL(url)
 	}
-
-	cr := &CacheReader{Inner: r, Size: r.Size}
-
-	zr, err := zip.NewReader(cr, cr.Size)
-	if err != nil {
-		log.Printf("Couldn't open zip file: %v", err)
-		return
-	}
-
-	fs := make([]*zip.File, len(zr.File))
-	copy(fs, zr.File)
-	sort.Sort(sortableZIPList(fs))
-
-	go func() {
-		for _, f := range fs {
-			log.Printf("adding file %v", f.Name)
-			addFile(f)
-		}
-	}()
 }
